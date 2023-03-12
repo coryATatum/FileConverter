2b6970f525d4a4e1d2b5824b09d9d14a471bcf34
coryATatum <tatumstew223@gmail.com>
2023-03-11 21:35:37 -0600

    edits

---
+++ index.js
@@ -0,0 +1,7 @@
+const browseBtn = document.getElementById('browse-btn');
+  browseBtn.addEventListener('click', () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.click();
+  });
+
