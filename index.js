b6d8d23782e1a98f812d978cb4df729c2b91fd3e
coryATatum <tatumstew223@gmail.com>
2023-03-14 21:27:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const browseBtn = document.getElementById('browse-btn');
-  browseBtn.addEventListener('click', () => {
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.click();
-  });
+browseBtn.addEventListener('click', () => {
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.click();
+});
 
-///////////
